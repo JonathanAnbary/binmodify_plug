83163ae58c44e39be0031d5968b4c02c32e19594
Jonathan Anbary <jonathananbary@gmail.com>
2025-03-05 22:03:23 +0200

    wip

--- build.zig
+++ build.zig
@@ -1,29 +1,24 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    // This creates a "module", which represents a collection of source files alongside
-    // some compilation options, such as optimization mode and linked system libraries.
-    // Every executable or library we compile will be based on one or more modules.
+    cosnt idasdk = b.option([]const u8, "idasdk", "relative path to installation of the ida sdk") orelse return error.MustProvideIdaSdk;
+    const idasdkpath = b.path(idasdk);
+
+    const libdir = switch (target.result.os.tag) {
+        .linux => switch (target.result.cpu.arch) {
+            .x86 => "x64_linux_gcc_32",
+            .x86_64 => "x64_linux_gcc_64",
+            else => return error.ArchNotSupported,
+        },
+        else => return error.OsNotSupported,
+    };
+
     const lib_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
+        .roo
         .target = target,
         .optimize = optimize,
     });
